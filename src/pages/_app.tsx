81cb202552b66e74bd5ca9a2ba855f3db10d499e
sagar soni <sagar.soni@gsr-inc.com>
2025-04-19 00:41:14 +0530

    Analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,12 @@ import type { AppProps } from "next/app";
 import { poppins, sourceSans } from "@/fonts";
 import Layout from "./layout";
 import classNames from "classnames";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={classNames(poppins.variable, sourceSans.variable)}>
       <Layout>
+        <Analytics/>
         <Component {...pageProps} />
       </Layout>
     </main>
