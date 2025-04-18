66aa92ff7a46f514145741a0ced72ce8ecb7e4a7
sagar soni <sagar.soni@gsr-inc.com>
2025-04-19 00:46:44 +0530

    speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,14 @@ import { poppins, sourceSans } from "@/fonts";
 import Layout from "./layout";
 import classNames from "classnames";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={classNames(poppins.variable, sourceSans.variable)}>
       <Layout>
         <Analytics/>
+        <SpeedInsights/>
         <Component {...pageProps} />
       </Layout>
     </main>
